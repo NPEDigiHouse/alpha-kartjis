caa3784a984c40474c36e48365e29698b52a5e2c
alba054 <prakasamas19h@student.unhas.ac.id>
2023-04-24 14:05:00 +0800

    create hash when successful callback obtained

--- src/services/callback/callback.ts
+++ src/services/callback/callback.ts
@@ -1,11 +1,28 @@
 import { BadRequestError } from "../../exceptions/BadRequestError";
 import { Order } from "../../models/Order";
+import { v4 as uuidv4 } from "uuid";
+import { hashData } from "../../utils";
+import { TicketVerification } from "../../models/TicketVerification";
+
+interface OrderDetail {
+  orderDetailId: string;
+  name: string;
+  birthDate: number;
+  phoneNumber: string;
+  invoice: string;
+  ticketId: string;
+  orderDate: Date;
+  gender: string;
+  timestamp: number;
+}
 
 export class CallbackService {
   orderModel: Order;
+  ticketVerificationModel: TicketVerification;
 
   constructor() {
     this.orderModel = new Order();
+    this.ticketVerificationModel = new TicketVerification();
   }
 
   async verifyPayment(data: any, token: any) {
@@ -24,10 +41,38 @@ export class CallbackService {
     if (data.status === "SUCCESSFUL") {
       console.log("berhasil");
 
-      await this.orderModel.changePaymentStatusByBillId(
+      const order = await this.orderModel.changePaymentStatusByBillId(
         data.bill_link_id,
         true
       );
+
+      if (order) {
+        for (let i = 0; i < order.orderDetails.length; i++) {
+          const id = uuidv4();
+          const orderDetail = order.orderDetails[i];
+          const data = {
+            orderDetailId: orderDetail.id,
+            timestamp: Date.now(),
+            birthDate: orderDetail.birthDate,
+            gender: orderDetail.gender,
+            invoice: orderDetail.orderId,
+            phoneNumber: orderDetail.phoneNumber,
+            name: orderDetail.name,
+            ticketId: orderDetail.ticketId,
+            orderDate: order.createdAt,
+          } as OrderDetail;
+          const hash = hashData(JSON.stringify(data));
+          console.log(data);
+
+          console.log(hash);
+
+          await this.ticketVerificationModel.addNewVerification(
+            id,
+            hash,
+            orderDetail.id
+          );
+        }
+      }
     }
   }
 }
