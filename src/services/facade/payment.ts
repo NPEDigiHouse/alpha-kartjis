08b994e96ee8e2c3d56be8a0e7d3b90ab8dc3304
alba054 <prakasamas19h@student.unhas.ac.id>
2023-05-28 13:06:17 +0800

    ticket total price = is adminFee + price

--- src/services/facade/payment.ts
+++ src/services/facade/payment.ts
@@ -34,7 +34,8 @@ export class PaymentService {
       for (let i = 0; i < order.orderDetails.length; i++) {
         const orderDetail = order.orderDetails[i];
         if (orderDetail.Ticket) {
-          amount += orderDetail.Ticket?.price;
+          amount +=
+            orderDetail.Ticket?.price + (orderDetail.Ticket.adminFee ?? 0);
         }
       }
     }
