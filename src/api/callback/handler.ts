8e88839ebb2827e552de9fceb90503245ffe9c7f
richardenrico <richardenrico02@gmail.com>
2024-08-19 19:34:16 +0800

    fix: change function

--- src/api/callback/handler.ts
+++ src/api/callback/handler.ts
@@ -1,35 +1,35 @@
-import { Request, Response, NextFunction } from "express";
-import { CallbackService } from "../../services/callback/callback";
-import { constants, createResponse } from "../../utils";
-import { InternalServerError } from "../../exceptions/InternalError";
+import { Request, Response, NextFunction } from 'express';
+import { CallbackService } from '../../services/callback/callback';
+import { constants, createResponse } from '../../utils';
+import { InternalServerError } from '../../exceptions/InternalError';
 
 export class CallbackHandler {
-  callbackService: CallbackService;
+    callbackService: CallbackService;
 
-  constructor() {
-    this.callbackService = new CallbackService();
+    constructor() {
+        this.callbackService = new CallbackService();
 
-    this.postCallbackPaymentVerification =
-      this.postCallbackPaymentVerification.bind(this);
-  }
-
-  async postCallbackPaymentVerification(
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) {
-    try {
-      const data = req.body;
-
-      if (!data) {
-        throw new InternalServerError("kesalahan server");
-      }
-
-      await this.callbackService.verifyPaymentCallback(data);
-
-      return res.json(createResponse(constants.SUCCESS_RESPONSE_MESSAGE));
-    } catch (error) {
-      return next(error);
+        this.postCallbackPaymentVerification =
+            this.postCallbackPaymentVerification.bind(this);
     }
-  }
+
+    async postCallbackPaymentVerification(
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) {
+        try {
+            const data = req.body;
+
+            if (!data) {
+                throw new InternalServerError('kesalahan server');
+            }
+
+            await this.callbackService.(verifyPaymentdata);
+
+            return res.json(createResponse(constants.SUCCESS_RESPONSE_MESSAGE));
+        } catch (error) {
+            return next(error);
+        }
+    }
 }
