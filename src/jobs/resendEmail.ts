947b7b42edae39c3218e21e65ed601f6ff281f86
richardenrico <richardenrico02@gmail.com>
2024-10-10 17:57:32 +0800

    fix: minor changes

--- src/jobs/resendEmail.ts
+++ src/jobs/resendEmail.ts
@@ -1,36 +1,36 @@
-import path from "path";
-import { config } from "../config";
-import pug from "pug";
-import { EmailHelper } from "../helper/EmailHelper";
-import { Order } from "../models/Order";
+import path from 'path';
+import { config } from '../config';
+import pug from 'pug';
+import { EmailHelper } from '../helper/EmailHelper';
+import { Order } from '../models/Order';
 
 const main = async () => {
-  const orderModel = new Order();
-  const order = await orderModel.getOrderById("");
+    const orderModel = new Order();
+    const order = await orderModel.getOrderById('');
 
-  order?.orderDetails.forEach((od) => {
-    const clientUrl = `https://kartjis.id/my-ticket/info/${od.id}`;
-    const emailBody = {
-      from: config.config().KARTJIS_MAIL,
-      to: od.email,
-      subject: `E-Tiket [${order.Event?.name}] - ${od.name}`,
-      // html: `<a href="${clientUrl}">${clientUrl}</a>`,
-      html: pug.compileFile(
-        path.join(__dirname, "..", "..", "views/email.pug")
-      )({
-        name: od.name,
-        ticketName: od.Ticket?.name,
-        orderNumber: order.id,
-        orderDate: new Date(order.createdAt),
-        paymentMethod: "other",
-        redirectLink: clientUrl,
-      }),
-      text: "",
-    };
+    order?.orderDetails.forEach((od) => {
+        const clientUrl = `https://kartjis.id/my-ticket/info/${od.id}`;
+        const emailBody = {
+            from: config.config().KARTJIS_MAIL,
+            to: od.email,
+            subject: `E-Kartjis [${order.Event?.name}] - ${od.name}`,
+            // html: `<a href="${clientUrl}">${clientUrl}</a>`,
+            html: pug.compileFile(
+                path.join(__dirname, '..', '..', 'views/email.pug')
+            )({
+                name: od.name,
+                ticketName: od.Ticket?.name,
+                orderNumber: order.id,
+                orderDate: new Date(order.createdAt),
+                paymentMethod: 'other',
+                redirectLink: clientUrl,
+            }),
+            text: '',
+        };
 
-    const emailHelper = new EmailHelper();
-    emailHelper.sendEmail(emailBody);
-  });
+        const emailHelper = new EmailHelper();
+        emailHelper.sendEmail(emailBody);
+    });
 };
 
 main();
