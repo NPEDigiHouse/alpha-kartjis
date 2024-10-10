20eee306cb080e9125b9356c4063983402c41f84
alba054 <jralif02@gmail.com>
2024-10-10 18:26:50 +0700

    give random interval for sending email from 1 - 10 minutes

--- src/services/database/order.ts
+++ src/services/database/order.ts
@@ -1,65 +1,66 @@
-import { config } from '../../config';
-import { BadRequestError } from '../../exceptions/BadRequestError';
-import { NotFoundError } from '../../exceptions/NotFoundError';
-import { EmailHelper } from '../../helper/EmailHelper';
-import { Order } from '../../models/Order';
-import { OrderDetailMapper } from '../../utils/dto/order';
-import pug from 'pug';
-import path from 'path';
+import { config } from "../../config";
+import { BadRequestError } from "../../exceptions/BadRequestError";
+import { NotFoundError } from "../../exceptions/NotFoundError";
+import { EmailHelper } from "../../helper/EmailHelper";
+import { Order } from "../../models/Order";
+import { OrderDetailMapper } from "../../utils/dto/order";
+import pug from "pug";
+import path from "path";
 
 export class OrderService {
-    model: Order;
+  model: Order;
+  
+  constructor() {
+    this.model = new Order();
+  }
 
-    constructor() {
-        this.model = new Order();
+  async sendEmailToOder(orderId: string) {
+    const order = await this.model.getOrderById(orderId);
+
+    if (!order) {
+      throw new NotFoundError("order's not found");
     }
 
-    async sendEmailToOder(orderId: string) {
-        const order = await this.model.getOrderById(orderId);
-
-        if (!order) {
-            throw new NotFoundError("order's not found");
-        }
-
-        if (order.status === 'FAILED' || order.status === 'INPROCESS') {
-            throw new BadRequestError('order has not been paid');
-        }
-
-        order?.orderDetails.forEach((od) => {
-            const clientUrl = `https://kartjis.id/my-ticket/info/${od.id}`;
-            const emailBody = {
-                from: config.config().KARTJIS_MAIL,
-                to: od.email,
-                subject: `E-Kartjis [${order.Event?.name}] - ${od.name}`,
-                // html: `<a href="${clientUrl}">${clientUrl}</a>`,
-                html: pug.compileFile(
-                    path.join(__dirname, '..', '..', '..', 'views/email.pug')
-                )({
-                    name: od.name,
-                    ticketName: od.Ticket?.name,
-                    orderNumber: order.id,
-                    orderDate: new Date(order.createdAt),
-                    paymentMethod: 'other',
-                    redirectLink: clientUrl,
-                }),
-                text: '',
-            };
-
-            const emailHelper = new EmailHelper();
-            setTimeout(() => {
-                emailHelper.sendEmail(emailBody);
-            }, 5 * 60 * 1000);
-            // emailHelper.sendEmail(emailBody);
-        });
+    if (order.status === "FAILED" || order.status === "INPROCESS") {
+      throw new BadRequestError("order has not been paid")
     }
 
-    async getOrderDetail(orderId: string) {
-        const order = await this.model.getOrderById(orderId);
+    
+  order?.orderDetails.forEach((od) => {
+    const clientUrl = `https://kartjis.id/my-ticket/info/${od.id}`;
+    const emailBody = {
+      from: config.config().KARTJIS_MAIL,
+      to: od.email,
+      subject: `E-Tiket [${order.Event?.name}] - ${od.name}`,
+      // html: `<a href="${clientUrl}">${clientUrl}</a>`,
+      html: pug.compileFile(
+        path.join(__dirname, "..", "..", "..", "views/email.pug")
+      )({
+        name: od.name,
+        ticketName: od.Ticket?.name,
+        orderNumber: order.id,
+        orderDate: new Date(order.createdAt),
+        paymentMethod: "other",
+        redirectLink: clientUrl,
+      }),
+      text: "",
+    };
 
-        if (!order) {
-            throw new NotFoundError("order's not found");
-        }
+    const emailHelper = new EmailHelper();
+    setTimeout(() => {
+      emailHelper.sendEmail(emailBody);
+    }, (Math.floor(Math.random() * (10 - 1 + 1)) + 1) * 60 * 1000);
+    // emailHelper.sendEmail(emailBody);
+  });
+  }
 
-        return OrderDetailMapper(order);
+  async getOrderDetail(orderId: string) {
+    const order = await this.model.getOrderById(orderId);
+
+    if (!order) {
+      throw new NotFoundError("order's not found");
     }
+
+    return OrderDetailMapper(order);
+  }
 }
