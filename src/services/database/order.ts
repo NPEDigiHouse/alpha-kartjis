fe913ba13890d223d90c6f18773cf7e9b4f61e68
alba054 <jralif02@gmail.com>
2024-10-12 19:09:11 +0700

    fix bug

--- src/services/database/order.ts
+++ src/services/database/order.ts
@@ -10,7 +10,7 @@ import { TicketConstruction } from "../facade/ticketConstruction";
 
 export class OrderService {
   model: Order;
-  
+
   constructor() {
     this.model = new Order();
   }
@@ -26,35 +26,62 @@ export class OrderService {
       throw new BadRequestError("order has not been paid")
     }
 
-    const ticketConstruction = new TicketConstruction();
-    await ticketConstruction.composeTicket(order, "other");
+    // const ticketConstruction = new TicketConstruction();
+    // await ticketConstruction.composeTicket(order, "other");
 
-  // order?.orderDetails.forEach((od) => {
-  //   const clientUrl = `https://kartjis.id/my-ticket/info/${od.id}`;
-  //   const emailBody = {
-  //     from: config.config().KARTJIS_MAIL,
-  //     to: od.email,
-  //     subject: `E-Tiket [${order.Event?.name}] - ${od.name}`,
-  //     // html: `<a href="${clientUrl}">${clientUrl}</a>`,
-  //     html: pug.compileFile(
-  //       path.join(__dirname, "..", "..", "..", "views/email.pug")
-  //     )({
-  //       name: od.name,
-  //       ticketName: od.Ticket?.name,
-  //       orderNumber: order.id,
-  //       orderDate: new Date(order.createdAt),
-  //       paymentMethod: "other",
-  //       redirectLink: clientUrl,
-  //     }),
-  //     text: "",
-  //   };
+    for (let i = 0; i < order.orderDetails.length; i++) {
+      const orderDetail = order.orderDetails[i];
 
-  //   const emailHelper = new EmailHelper();
-  //   setTimeout(() => {
-  //     emailHelper.sendEmail(emailBody);
-  //   }, (Math.floor(Math.random() * (5 - 1 + 1)) + 1) * 60 * 1000);
-  //   // emailHelper.sendEmail(emailBody);
-  // });
+      const clientUrl = `https://kartjis.id/my-ticket/info/${orderDetail.id}`;
+      const emailBody = {
+        from: config.config().KARTJIS_MAIL,
+        to: orderDetail.email,
+        subject: `E-Tiket ${order.Event?.name} - ${orderDetail.name}`,
+        // html: `<a href="${clientUrl}">${clientUrl}</a>`,
+        html: pug.compileFile(
+          path.join(__dirname, "..", "..", "..", "views/email.pug")
+        )({
+          name: orderDetail.name,
+          ticketName: orderDetail.Ticket?.name,
+          orderNumber: order.id,
+          orderDate: new Date(order.createdAt),
+          paymentMethod: "other",
+          redirectLink: clientUrl,
+        }),
+        text: "",
+      };
+
+      const emailHelper = new EmailHelper();
+      setTimeout(() => {
+        emailHelper.sendEmail(emailBody);
+      }, (Math.floor(Math.random() * (5 - 1 + 1)) + 1) * 60 * 1000);
+    }
+    // order?.orderDetails.forEach((od) => {
+    //   const clientUrl = `https://kartjis.id/my-ticket/info/${orderDetail.id}`;
+    //   const emailBody = {
+    //     from: config.config().KARTJIS_MAIL,
+    //     to: orderDetail.email,
+    //     subject: `E-Tiket [${order.Event?.name}] - ${orderDetail.name}`,
+    //     // html: `<a href="${clientUrl}">${clientUrl}</a>`,
+    //     html: pug.compileFile(
+    //       path.join(__dirname, "..", "..", "..", "views/email.pug")
+    //     )({
+    //       name: orderDetail.name,
+    //       ticketName: orderDetail.Ticket?.name,
+    //       orderNumber: order.id,
+    //       orderDate: new Date(order.createdAt),
+    //       paymentMethod: "other",
+    //       redirectLink: clientUrl,
+    //     }),
+    //     text: "",
+    //   };
+
+    //   const emailHelper = new EmailHelper();
+    //   setTimeout(() => {
+    //     emailHelper.sendEmail(emailBody);
+    //   }, (Math.floor(Math.random() * (5 - 1 + 1)) + 1) * 60 * 1000);
+    //   // emailHelper.sendEmail(emailBody);
+    // });
   }
 
   async getOrderDetail(orderId: string) {
